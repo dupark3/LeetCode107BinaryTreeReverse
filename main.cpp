b099899c5a0ac1206d490c9feac25f98e646d82c
DuParkLinux <dupark3@gmail.com>
2018-04-18 07:12:17 -0400

    First accepted version. 6ms runtime

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+
 class Solution {
 public:
     // recursively add
@@ -23,7 +24,7 @@ public:
             --level;
         }
         if(node->right){
-            read(node->right, ++level);
+            read(node->right, ret, ++level);
             --level;
         }
     }
